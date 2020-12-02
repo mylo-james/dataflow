f3b0d9a04f7ed701ea14ae25895089f3aff3d974
Mylo James <mjames@appacademy.io>
2020-12-02 14:27:06 -0600

    normalize and favicon

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom';
 import { StrictMode } from 'react';
 import './index.css';
+import './normalize.css';
 import App from './App';
 import configureStore from './store';
 
 // Attach store to Application
 configureStore();
 
-//Attach React Application to HTML 
+//Attach React Application to HTML
 ReactDOM.render(
     <StrictMode>
         <App />
