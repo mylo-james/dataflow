5f877017a14ab3a020e380f22a536fa5d4e5a6a5
Mylo James <mjames@appacademy.io>
2020-12-16 08:52:46 -0600

    changed store to look like projects

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,6 +3,7 @@ import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
 
 //Middleware Imports
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 //Reducers
 import userReducer from './userReducer';
@@ -12,13 +13,19 @@ const rootReducer = combineReducers({
     users: userReducer,
 });
 
-// Allow Redux tools
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let enhancer;
+// Production only needs thunk
+if (process.env.NODE_ENV === 'production') {
+    enhancer = applyMiddleware(thunk);
+} else {
+    // Dev we need devtools and logger
+    const composeEnhancers =
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
 
-// Apply middleware
-const storeEnhancer = composeEnhancers(applyMiddleware(thunk));
-
-// Define Store
-const configureStore = () => createStore(rootReducer, storeEnhancer);
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+};
 
 export default configureStore;
