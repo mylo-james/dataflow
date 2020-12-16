73583754344a51c210912760427ab2bc0a113646
Mylo James <mjames@appacademy.io>
2020-12-16 16:28:44 -0600

    removed routes

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-// Define Router
-const router = require('express').Router();
-// Imports
-const asyncHandler = require('express-async-handler');
-
-// Endpoint Definitions
-
-// Export Router
-module.exports = router;
