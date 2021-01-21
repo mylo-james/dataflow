4f7ad4c7df7bc8ca4c73dc06da3a52ac09d22005
Mylo James <mjames@appacademy.io>
2021-01-21 14:13:47 -0600

    fixed router and name

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,5 @@
 /* No need to open this file for lecture. */
-const router = require('express').Router;
+const router = require('express').Router();
 const groupRoutes = require('./groups');
 const instructorRoutes = require('./instructors');
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 /* No need to open this file for lecture. */
-const router = require('express').Router;
+const router = require('express').Router();
 const apiRoutes = require('./api');
 
-router.use('./api', apiRoutes);
+// router.use('./api', apiRoutes);
 
 module.exports = router;
